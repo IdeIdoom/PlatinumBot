b0305faec87155b88db4a0db8e2f51ae628101a4
IdeIdoom <ideidoom@gmail.com>
2023-06-27 15:16:06 +0200

    Small fixes

--- PlatinumBot/Data/EightBall.cs
+++ PlatinumBot/Data/EightBall.cs
@@ -2,10 +2,10 @@ namespace PlatinumBot.Data;
 
 public class EightBallResponse
 {
-    public string? Response { get; set; }
+    public string Response { get; set; } = string.Empty;
 }
 
 public class EightBallResponses
 {
-    public List<EightBallResponse>? Responses { get; set; }
+    public List<EightBallResponse> Responses { get; set; } = new List<EightBallResponse>();
 }
--- PlatinumBot/Services/DbService.cs
+++ PlatinumBot/Services/DbService.cs
@@ -5,12 +5,11 @@ namespace PlatinumBot.Services;
 
 public class DbService 
 {
-    public IDictionary<String, ArknightsOperator> ArknightsOperators { get; private set; }
-    public List<String> EightBallResponses {get; private set;}
+    public IDictionary<String, ArknightsOperator> ArknightsOperators { get; private set; } = new Dictionary<String, ArknightsOperator>(); 
+    public List<String> EightBallResponses {get; private set;} = new List<String>();    
     public DbService()
     {
-        ArknightsOperators = new Dictionary<String, ArknightsOperator>();
-        EightBallResponses = new List<String>();
+
     }
 
     public void Init() 
@@ -19,17 +18,25 @@ public class DbService
         try 
         {
             jsonLoader.Load("./data/eightball.json");
-            EightBallResponses? responses = JsonConvert.DeserializeObject<EightBallResponses>(jsonLoader.JsonData);
-            foreach(var response in responses.Responses)
+            if(jsonLoader.JsonData is not null && jsonLoader.JsonData != string.Empty)
             {
-                EightBallResponses.Add(response.Response);
+                EightBallResponses? responses = JsonConvert.DeserializeObject<EightBallResponses>(jsonLoader.JsonData);
+                if(responses is not null)
+                {
+                    foreach(var response in responses.Responses)
+                    {
+                        EightBallResponses.Add(response.Response);
+                    }
+                }
             }
+            
+            
             //jsonLoader.Load("./data/operator_overviews.json");
             //We do stuff here for ops
         }
         catch(Exception ex) 
         {
-
+            throw new Exception(ex.Message);
         }
             
     }
@@ -37,7 +44,7 @@ public class DbService
 
 public class JSONLoader
 {
-    public string? JsonData {get; private set;}
+    public string JsonData {get; private set;} = string.Empty;
     public void Load(string path)
     {
         using StreamReader streamReader = new(path);
